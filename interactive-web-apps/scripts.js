fe337fe01ab6a267659197f7fb3db7737bd8ab13
maurenskie <126791873+maurenskie@users.noreply.github.com>
2023-04-20 16:33:29 +0200

    Update scripts.js

--- interactive-web-apps/scripts.js
+++ interactive-web-apps/scripts.js
@@ -1,16 +1,26 @@
+const MAX_NUMBER = 15
+const MIN_NUMBER = -5
+const STEP_AMOUNT = 5;
+
 const number = document.querySelector('[data-key="number"]')
-const subtract = document.querySelector('[data-key="subtract"]')
-const add = document.querySelector('[data-key="add"]')
+const subtract = document.querySelector('[data-key="subtract"')
+const add = document.querySelector('[data-key="subtract"]')
 
 const subtractHandler = () => {
-    console.log(number.value)
-    number.value =number.value -1;
+    const newValue = parseInt(number.value) - STEP_AMOUNT
+    number.value = newValue
+
+    if (add.disabled === true) {
+        add.disabled = false
+    }
+
+    if(newValue <= MIN_NUMBER) {
+        subtract.disabled = true
+    }
 }
+
 
 const addHandler = () => {
-    number.value = number.value + 1;
+    const newValue = parseInt(number.value) + STEP_AMOUNT
+    number.value = newValue
 }
-
-
-subtract.addEventListener('click', subtractHandler)
-add.addEventListener('click', addHandler)
